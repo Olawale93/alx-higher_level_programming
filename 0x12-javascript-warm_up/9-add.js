9372396e6a0f242bf9a71914f897b586f5de170c
Olawale93 <olawaleadeoluwa@gmail.com>
2024-06-20 01:48:16 -0700

    jave

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,11 +3,4 @@ function add (a, b) {
   return a + b;
 }
 
-const a = parseInt(process.argv[2], 10);
-const b = parseInt(process.argv[3], 10);
-
-if (isNaN(a) || isNaN(b)) {
-  console.log('Both arguments must be integers');
-} else {
-  console.log(add(a, b));
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));

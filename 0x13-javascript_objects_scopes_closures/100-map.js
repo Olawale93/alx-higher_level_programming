8541d2275edafb83e7f9efe731b6d9572205c8a1
Olawale93 <olawaleadeoluwa@gmail.com>
2024-06-20 15:29:39 -0700

    scopes

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const data = require('./100-data.js');
+const list = data.list;
+
+const newList = list.map((value, index) => value * index);
+
+console.log(list);
+console.log(newList);

0dcf52aae8b973007e03aafd09c7c5873a0c69ad
Olawale93 <olawaleadeoluwa@gmail.com>
2024-06-20 14:30:13 -0700

    scopes

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,34 +1,18 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0) {
-            // If width or height is not a positive integer, create an empty object
-            this.width = 0;
-            this.height = 0;
-        } else {
-            this.width = w;
-            this.height = h;
-        }
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-    print() {
-        if (this.width === 0 || this.height === 0) {
-            console.log("Empty Rectangle");
-        } else {
-            for (let i = 0; i < this.height; i++) {
-                console.log('X'.repeat(this.width));
-            }
-        }
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
 
-    rotate() {
-        // Exchange width and height
-        [this.width, this.height] = [this.height, this.width];
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
 
-    double() {
-        // Double width and height
-        this.width *= 2;
-        this.height *= 2;
-    }
-}
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+};
